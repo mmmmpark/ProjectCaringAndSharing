30d481d9f1fc0d65839b42289933703adf41b533
Marissa Novak <marissa_novak@hmc.edu>
2014-03-05 00:38:54 -0800

    Fixed slideshow, fixed links on navbar, made homepage

--- script.js
+++ script.js
@@ -27,3 +27,24 @@ function changeDiv(target)
 	}
 	document.getElementById(target).style.display = "";
 }
+
+function slideSwitch() {
+    var $active = $('#slideshow IMG.active');
+
+    if ( $active.length == 0 ) $active = $('#slideshow IMG:last');
+
+    var $next =  $active.next().length ? $active.next()
+        : $('#slideshow IMG:first');
+
+    $active.addClass('last-active');
+        
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 1000, function() {
+            $active.removeClass('active last-active');
+        });
+}
+
+$(function() {
+    setInterval( "slideSwitch()", 5000 );
+});
